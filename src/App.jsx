92e0577df5b8f6f97c23de3bb28de47db503211b
Fabiola Ocampo <fabieocampo17qgmail.com>
2023-11-13 21:49:27 -0300

    PReEntrega2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { NavBar } from "./components/NavBar";
+import { ItemsListContainer } from "./components/ItemsListContainer";
+import "./App.css";
+
+function App() {
+  return (
+  <>
+  <NavBar/>
+  <ItemsListContainer greeting="Articulos de Cerámica"/>
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import cart from '../assets/carrito-de-compras.png'
+
+export const CartWidget = () => {
+    return (
+    <>
+    <img src={cart} alt="Carrito de compras" width={30}/>
+    <span>4</span>
+    </>
+    );
+
+};
